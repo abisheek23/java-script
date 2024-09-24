12812b945090d89fbd76207792747e8e7c31de48
abisheek23 <abisheek2397@gmail.com>
2024-09-24 11:27:50 +0530

    js

---
+++ .vscode/dictionaryorobject/script.js
@@ -0,0 +1,40 @@
+d=[{name:'arun',age:20,place:'ekm'},{name:'anu',age:30,place:'ekm'},{name:'sanu',age:25,place:'ekm'},{name:'arun',age:28,place:'ekm'},{name:'arun',age:30,place:'ekm'},{name:'arun',age:35,place:'ekm'},{name:'arun',age:40,place:'ekm'}]
+console.log(d);
+ a=[]
+ b=[]
+ for (i of d){
+    if (i.age>=30){
+  
+
+a.push(i)
+
+    }
+else 
+b.push(i)
+
+
+ }
+ console.log(i);
+ 
+ console.log(a);
+ console.log(b);
+ 
+ 
+
+ 
+// console.log(d);
+// console.log(d.age);
+// d.place='thr'
+// console.log(d);
+// d.lang='mal'
+// console.log(d);
+// delete d.lang
+// console.log(d);
+//  for (let i in d){
+//     if (d[i]==30){
+//         console.log(i,d[i]);
+        
+//     }
+//  }
+
+
---
+++ .vscode/work1/script.js
@@ -0,0 +1,5 @@
+d=[{name:'arun',age:20,place:'ekm'},{name:'anu',age:30,place:'ekm'},{name:'sanu',age:25,place:'ekm'},{name:'arun',age:28,place:'ekm'},{name:'arun',age:30,place:'ekm'},{name:'arun',age:35,place:'ekm'},{name:'arun',age:40,place:'ekm'}]
+d.forEach(element => {
+    console.log(element.name);
+    
+});

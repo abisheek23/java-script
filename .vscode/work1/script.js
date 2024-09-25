c497612a8fcb0c9f48a8bac114105653f67fbf4e
abisheek23 <abisheek2397@gmail.com>
2024-09-25 11:24:23 +0530

    js

--- .vscode/work1/script.js
+++ .vscode/work1/script.js
@@ -1,5 +1,38 @@
-d=[{name:'arun',age:20,place:'ekm'},{name:'anu',age:30,place:'ekm'},{name:'sanu',age:25,place:'ekm'},{name:'arun',age:28,place:'ekm'},{name:'arun',age:30,place:'ekm'},{name:'arun',age:35,place:'ekm'},{name:'arun',age:40,place:'ekm'}]
+d=[{name:'arun',age:20,place:'ekm'},{name:'anu',age:30,place:'thr'},{name:'joji',age:25,place:'kol'},{name:'arathi',age:28,place:'pal'},{name:'alex',age:30,place:'koz'},{name:'sree',age:35,place:'pat'},{name:'manu',age:40,place:'alp'}]
+function display(){
+    const t_var=document.querySelector('#peopleTable tbody')
+    t_var.innerHTML=''
+
 d.forEach(element => {
-    console.log(element.name);
+      const t_row=document.createElement('tr')
+
+      const t_name=document.createElement('td')
+      t_name.textContent=element.name
+      t_row.appendChild(t_name)
+
+      const t_age=document.createElement('td')
+      t_age.textContent=element.age
+      t_row.appendChild(t_age)
+
+      const t_place=document.createElement('td')
+      t_place.textContent=element.place
+      t_row.appendChild(t_place)
+
+      t_var.appendChild(t_row)
+
     
 });
+}
+document.getElementById('Submit_form').addEventListener('submit',function(event){
+    event.preventDefault()  
+    const name=document.getElementById('name').value 
+    const age=parseInt(document.getElementById('age').value )
+    const place=document.getElementById('place').value 
+    d.push({name:name,age:age,place:place})
+    document.getElementById('name').value=''
+    document.getElementById('age').value=''
+    document.getElementById('place').value=''
+
+    display()
+})
+display()
